a1ff25724efa134b2d782a775cccdf4bba80e247
higor <hstuquifatec@gmail.com>
2024-08-14 20:34:16 -0300

    docs: created new directory for lessons

---
+++ projetos-e-codigos/java-anatomia-classes/src/edu/higor/sintaxebasica/MinhaClasse.java
@@ -0,0 +1,16 @@
+package edu.higor.sintaxebasica;
+
+public class MinhaClasse {
+    public static void main (String [] args) {
+    String primeiroNome = "Higor";
+    String segundoNome = "Stuqui";
+    
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+    System.out.println(nomeCompleto);
+}
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
+
+}
